a66e00a98df9077a8722868a1e5af67bf5c4f7af
Alex Hemming <alexandra@ada.ac.uk>
2018-04-24 11:45:48 +0100

    Add files via upload

---
+++ Bubble Sort Worksheet.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# # Bubble Sort
+# ## Pseudocode
+# ```
+# procedure bubbleSort(list)
+#    output list
+#    for i = 0 to range length list -1, 0, -1 do:		
+#       for j = 0 to range i do:
+#          if list[j] > list[j+1] then
+#             temp = list[j]
+#             list[j] = list[j+1]
+#             list[j+1] = temp
+#          end if        
+#       end for 
+#    end for
+#    output list
+# end procedure 
+# ```
